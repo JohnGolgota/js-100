8056276e31ff8028ff818ea606a333395b24f0b5
JohnGolgota <104475729+JohnGolgota@users.noreply.github.com>
2024-09-24 14:32:53 -0500

    solo por probar

--- index.js
+++ index.js
@@ -20,19 +20,19 @@ const projects = [
 ]
 
 projects.forEach((project) => {
-  const $project = document.createElement('div')
+  const $project = document.createElement('section')
   $project.classList.add('project')
   $project.innerHTML = `
     <div class="project__header">
       <h2 class="project__title">${project.name}</h2>
       <p class="project__description">${project.description}</p>
     </div>
-    <div class="project__tags">
-      ${project.tags.map((tag) => `<span class="project__tag">${tag}</span>`).join('')}
-    </div>
-    <div class="project__links">
-      ${project.links.map((link) => `<a class="project__link" href="${link.url}">${link.name}</a>`).join('')}
-    </div>
+    <ul class="project__tags">
+      ${project.tags.map((tag) => `<li class="project__tag">${tag}</li>`).join('')}
+    </ul>
+    <ul class="project__links">
+      ${project.links.map((link) => `<li><a class="project__link" href="${link.url}">${link.name}</a></li>`).join('')}
+    </ul>
   `
   document.querySelector('.projects').appendChild($project)
 })
